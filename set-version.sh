4a03d4a6d710534017bef6446cfba933ac9e93b6
James Woolfenden <james.woolfenden@gmail.com>
2023-06-15 16:06:30 +0100

    fix lint

--- set-version.sh
+++ set-version.sh
@@ -2,5 +2,5 @@
 latesttag=$(git describe --tags)
 echo "Updating version file with new tag: $latesttag"
 echo "package sato" > src/version.go
-echo "" >> src/version.go
-echo "const Version = \"$latesttag\"" >> src/version.go
+echo "" >> src/cf/version.go
+echo "const Version = \"$latesttag\"" >> src/cf/version.go
