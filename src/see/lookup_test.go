840236ce05b31a835f0f470db926ab0ea687157b
James Woolfenden <james.woolfenden@gmail.com>
2024-09-24 11:11:41 +0100

    add new resources lookups and method for finding missing, added new default of none for lookup items that do exist but have no partner

--- src/see/lookup_test.go
+++ src/see/lookup_test.go
@@ -1,8 +1,15 @@
 package see
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"testing"
+
+	"github.com/rs/zerolog/log"
 )
 
 func TestLookup(t *testing.T) {
@@ -16,6 +23,7 @@ func TestLookup(t *testing.T) {
 	result := "aws_appautoscaling_target"
 	myServiceBus := "azurerm_servicebus_namespace"
 	reverse := "aws::efs::filesystem"
+	none := "none"
 
 	tests := []struct {
 		name    string
@@ -24,6 +32,7 @@ func TestLookup(t *testing.T) {
 		wantErr bool
 	}{
 		{name: "Pass", args: args{"AWS::ApplicationAutoScaling::ScalableTarget", false}, want: &result, wantErr: false},
+		{name: "None", args: args{"alexa::ask::skill", false}, want: &none, wantErr: false},
 		{name: "Pass", args: args{"Microsoft.ServiceBus/namespaces/", false}, want: &myServiceBus, wantErr: false},
 		{name: "Fail", args: args{"AWS::Guff::Guffing", false}, want: nil, wantErr: true},
 		{name: "Reverse", args: args{resource: "aws_efs_file_system", reverse: true}, want: &reverse, wantErr: false},
@@ -47,4 +56,49 @@ func TestLookup(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestLookupAll(t *testing.T) {
+	t.Parallel()
+	directory := "../../schema"
+	files, err := os.ReadDir(directory)
+
+	if err != nil {
+		log.Warn().Msgf("failed to find files at  %s", directory)
+	}
+
+	for _, file := range files {
+		//has file extension json
+		//has lookup
+		if strings.Contains(file.Name(), ".json") {
+			fileName := filepath.Join(directory, file.Name())
+			content, err := os.ReadFile(fileName)
+			if err != nil {
+				log.Warn().Msgf("file is empty %s", file.Name())
+			}
+
+			var result map[string]interface{}
+			err = json.Unmarshal(content, &result)
+
+			if err != nil {
+				log.Error().Msgf("failed to parse %s", fileName)
+				return
+			}
+
+			typeName := strings.ToLower(result["typeName"].(string))
+			_, err = Lookup(typeName, false)
+			if err != nil {
+
+				var s strings.Builder
+				s.WriteString("\"")
+				s.WriteString(typeName)
+				s.WriteString("\": \"\",")
+				fmt.Println(s.String())
+				t.Errorf("Lookup incomplete")
+			}
+		}
+	}
+
+	//	got, err := Lookup(tt.args.resource, tt.args.reverse)
+
 }
